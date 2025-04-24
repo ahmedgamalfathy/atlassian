5e2af7a2419d7b4eb3f7fbfc14d50bb097e4b231
ahmed <ahmedgamalfathy39@gmail.com>
2025-04-24 11:31:43 +0200

    add 6 month to widgets

--- app/Http/Controllers/Api/Dashboard/Widget/WidgetController.php
+++ app/Http/Controllers/Api/Dashboard/Widget/WidgetController.php
@@ -19,7 +19,7 @@ class WidgetController extends Controller
         $monthlyReservations = Reservation::select(
             DB::raw('COUNT(*) as totalReservations, DATE_FORMAT(date, "%m-%Y") as month')
         )
-        ->where('date', '>=', Carbon::today()->subMonths(6))
+        ->where('date', '>=', Carbon::today()->subMonths(6)->startOfMonth())
         ->groupBy('month')
         ->orderBy('month')
         ->get();
