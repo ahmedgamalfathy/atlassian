eaa68ca352c45f57f7fb10d92e60188ad38294de
ahmed <ahmedgamalfathy39@gmail.com>
2025-05-07 15:12:00 +0300

    add select

--- app/Services/Select/ServiceWebsiteSelectService.php
+++ app/Services/Select/ServiceWebsiteSelectService.php
@@ -4,7 +4,7 @@ use App\Models\Services\Service;
 
 class ServiceWebsiteSelectService{
     public function getServices(){
-        $service = Service::where('is_active',1)->whereNotNull('schedule_id')->all(['id as value', 'title as label']);
+        $service = Service::where('is_active',1)->whereNotNull('schedule_id')->get(['id as value', 'title as label']);
         return $service;
     }
 
