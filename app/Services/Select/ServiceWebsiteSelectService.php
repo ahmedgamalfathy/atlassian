d31f9710418ad96bfe16a10492105322b8ecdd21
ahmed <ahmedgamalfathy39@gmail.com>
2025-05-07 15:15:07 +0300

    add fun to select

--- app/Services/Select/ServiceWebsiteSelectService.php
+++ app/Services/Select/ServiceWebsiteSelectService.php
@@ -3,7 +3,7 @@ namespace App\Services\Select;
 use App\Models\Services\Service;
 
 class ServiceWebsiteSelectService{
-    public function getServices(){
+    public function getWebsiteServices(){
         $service = Service::where('is_active',1)->whereNotNull('schedule_id')->get(['id as value', 'title as label']);
         return $service;
     }
